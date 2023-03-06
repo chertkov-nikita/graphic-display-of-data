f17575f521bbf107d13ec6df17b893051e7cf43f
chertkov-nikita <nikita.chertkov@yandex.ru>
2023-03-06 15:01:28 +0300

    Added registration

--- app/views.py
+++ app/views.py
@@ -1,10 +1,19 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.http import HttpResponseNotFound
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
 
 
 @login_required
-def app_page(request, graph_type='histogram'):
+def app_page(request, graph_type="histogram"):
     data = {"pages": [
         {"name": "Note", "id": "1"},
         {"name": "News", "id": "2"},
--- project/urls.py
+++ project/urls.py
@@ -5,5 +5,6 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/', views.SignUp.as_view(), name='signup'),
     path('display/<graph_type>/', views.app_page)
 ]
